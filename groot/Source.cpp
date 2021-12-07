d33b0cf0b6d15473df79fc3827ce6d097a045540
Wepeel <wepeelsuicune@gmail.com>
2021-12-07 10:14:43 +0200

    CR Fix errors

--- groot/Source.cpp
+++ groot/Source.cpp
@@ -2,18 +2,22 @@
 
 
 int main() {
-
 	int user_input = 0;
 	std::cout << "Please enter a number: ";
 	std::cin >> user_input;
 
-	if (user_input < 0) {
-		std::cout << "Number cannot be negative";
+	if (!std::cin) {
+		std::cout << "Error with reading input. Did you enter a number?\n";
 		return 1;
 	}
 
-	double sqrt = std::sqrt(user_input);
-	std::cout << "The square root of " << user_input << " is " << sqrt << std::endl;
+	if (user_input < 0) {
+		std::cout << "Number cannot be negative\n";
+		return 1;
+	}
+
+	double square_root = std::sqrt(user_input);
+	std::cout << "The square root of " << user_input << " is " << square_root << std::endl;
 
 	return 0;
 }
