d92e1f351d25d46906a7a806883e0ffe8cd337a1
Wepeel <wepeelsuicune@gmail.com>
2021-12-07 11:41:54 +0200

    CR Fix

--- optimus_prime/Source.cpp
+++ optimus_prime/Source.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 
-bool is_prime(int number) {
-	if (number < 2) {
+bool isPrime(unsigned number) {
+	if (number == 0 || number == 1) {
 		return false;
 	}
 
-	for (int i = 2; i < number; ++i) {
+	for (int i = 2; i < std::sqrt(number); ++i) {
 		if (number % i == 0) {
 			return false;
 		}
@@ -17,24 +17,25 @@ bool is_prime(int number) {
 
 
 int main() {
-	int number;
+	unsigned number;
 	std::cout << "Please enter a number: ";
 	std::cin >> number;
 
 	if (!std::cin) {
-		std::cout << "Failed to read number\n";
+		std::cout << "Failed to read number" << std::endl;
 		return 1;
 	}
 
 	if (number < 0) {
-		std::cout << "Number must be positive\n";
+		std::cout << "Number must be positive" << std::endl;
 		return 1;
 	}
 
-	if (is_prime(number)) {
-		std::cout << "Is prime\n";
-	} else {
-		std::cout << "Is not prime\n";
+	if (isPrime(number)) {
+		std::cout << "Is prime" << std::endl;
+	}
+	else {
+		std::cout << "Is not prime" << std::endl;
 	}
 
 	return 0;
